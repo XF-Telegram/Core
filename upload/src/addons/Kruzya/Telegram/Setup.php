328079b0f6e8be1784b40cb907e5c0fce65964c0
Kruzya <git@kruzya.me>
2018-10-27 10:03:47 +0400

    How we use StepRunnerTraits

--- upload/src/addons/Kruzya/Telegram/Setup.php
+++ upload/src/addons/Kruzya/Telegram/Setup.php
@@ -5,56 +5,81 @@ use XF\AddOn\AbstractSetup;
 use XF\Db\Schema\Create;
 use XF\Db\Schema\Alter;
 
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+
+/**
+ * TODO (before release 1.1)
+ *
+ * -> Add table prefix xf_
+ */
 class Setup extends AbstractSetup
 {
-  public function install(array $stepParams = []) {
-    $db = $this->db();
-    $db->insert('xf_connected_account_provider', [
-      'provider_id'           => 'telegram',
-      'provider_class'        => 'Kruzya\\Telegram:Provider\\Telegram',
-      'display_order'         => 150,
-      'options'               => '[]'
-    ]);
+	use StepRunnerInstallTrait;
+	use StepRunnerUpgradeTrait;
+	use StepRunnerUninstallTrait;
 
-    $sm = $db->getSchemaManager();
-    $sm->createTable('tg_user', function (Create $table) {
-      $table->addColumn('id',             'int')->primaryKey();
-      $table->addColumn('first_name',     'varchar', 64)->setDefault('');
-      $table->addColumn('last_name',      'varchar', 64)->setDefault('');
-      $table->addColumn('username',       'varchar', 32)->nullable();
-      $table->addColumn('photo_url',      'varchar', 256);
+	/**
+	 * Install
+	 */
+	public function installStep1() {
+		$this->db()->insert('xf_connected_account_provider', [
+			'provider_id'           => 'telegram',
+			'provider_class'        => 'Kruzya\\Telegram:Provider\\Telegram',
+			'display_order'         => 150,
+			'options'               => '[]'
+		]);
+	}
 
-      $table->addColumn('updated',        'int');
-    });
-  }
+	public function installStep2() {
+		$this->db->getSchemaManager()->createTable('tg_user', function (Create $table) {
+			$table->addColumn('id',             'int')->primaryKey();
+			$table->addColumn('first_name',     'varchar', 64)->setDefault('');
+			$table->addColumn('last_name',      'varchar', 64)->setDefault('');
+			$table->addColumn('username',       'varchar', 32)->nullable();
+			$table->addColumn('photo_url',      'varchar', 256);
 
-  public function upgrade(array $stepParams = []) {
-    $db = $this->db();
-    $sm = $db->getSchemaManager();
+			$table->addColumn('updated',        'int');
+		})
+	}
 
-    if (!$this->IsAddOnInstalled('Kruzya/TelegramNotifications')) {
-      $sm->alterTable('tg_user', function (Alter $table) {
-        if ($table->getColumnDefinition('notifications'))
-          $table->dropColumns(['notifications']);
-      });
-    }
+	/**
+	 * Upgrade
+	 */
+	public function upgrade1003070Step1() {
+		$this->db()->getSchemaManager()->alterTable('tg_user', function (Alter $table) {
+			$table->addColumn('notifications', 'bool')->setDefault(0)->after('photo_url');
+		});
+	}
 
-    $sm->dropTable('tg_notifications_queue');
-  }
+	public function upgrade1006070Step1() {
+		$this->db()->getSchemaManager()->createTable('tg_notifications_queue', function (Create $table) {
+			$table->addColumn('id',       'int')->autoIncrement();
+			$table->addColumn('receiver', 'int');
+			$table->addColumn('message',  'text');
+			$table->addColumn('marktype', 'enum')->values(['none', 'HTML', 'MarkDown'])->setDefault('none');
+			$table->addColumn('status',   'enum')->values(['planned', 'finished', 'failed'])->setDefault('planned');
+		});
+	}
 
-  public function uninstall(array $stepParams = []) {
-    $db = $this->db();
-    $sm = $db->getSchemaManager();
+	public function upgrade1007070Step1() {
+		$sm = $this->db()->getSchemaManager();
 
-    $db->delete('xf_connected_account_provider', "provider_id = 'telegram'");
-    $sm->dropTable('tg_user');
-  }
+		$sm->dropTable('tg_notifications_queue');
+		$sm->alterTable('tg_user', function (Alter $table) {
+			$table->dropColumns(['notifications']);
+		});
+	}
 
-  private function IsAddOnInstalled($ID) {
-    $AddOn = $this->app->em()->find('XF:AddOn', $ID);
-    if (!$AddOn)
-      return false;
+	/**
+	 * Uninstall
+	 */
+	public function uninstallStep1() {
+		$db = $this->db()->delete('xf_connected_account_provider', "provider_id = 'telegram'");
+	}
 
-    return $AddOn->active;
-  }
+	public function uninstallStep2() {
+		$this->db()->getSchemaManager()->dropTable('tg_user');
+	}
 }
