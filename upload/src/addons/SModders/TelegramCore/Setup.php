35e5731ad85740c3caf3f3df90c7e7f969bf13d1
Kruzya <kruzefag@gmail.com>
2019-09-01 23:37:18 +0400

    :sparkles: Start work on v2.0.0

---
+++ upload/src/addons/SModders/TelegramCore/Setup.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is a part of [Telegram] Core.
+ * All rights reserved.
+ *
+ * Developed by SourceModders.
+ */
+
+namespace SModders\TelegramCore;
+
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+
+class Setup extends AbstractSetup
+{
+	use StepRunnerInstallTrait;
+	use StepRunnerUpgradeTrait;
+	use StepRunnerUninstallTrait;
+}
