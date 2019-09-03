8946a70599915db476a8b06ac9941ba174839b23
Kruzya <kruzefag@gmail.com>
2019-09-03 09:59:47 +0400

    :construction: Token verification before saving

--- upload/src/addons/SModders/TelegramCore/SubContainer/Telegram.php
+++ upload/src/addons/SModders/TelegramCore/SubContainer/Telegram.php
@@ -11,6 +11,7 @@ namespace SModders\TelegramCore\SubContainer;
 
 
 use TelegramBot\Api\BotApi;
+use TelegramBot\Api\Client;
 use XF\ConnectedAccount\Provider\AbstractProvider;
 use XF\Container;
 use XF\SubContainer\AbstractSubContainer;
@@ -80,13 +81,67 @@ class Telegram extends AbstractSubContainer
     {
         $container['api'] = function (Container $c)
         {
-            return function ($token)
+            return function ($token) use ($c)
             {
-                return new BotApi($token);
+                $api = new BotApi($token);
+                $api->setProxy($c['proxy']);
+                return $api;
             };
+        };
+
+        $container['client'] = function (Container $c)
+        {
+            return function ($token) use ($c)
+            {
+                $client = new Client($token);
+                $client->setProxy($c['proxy']);
+                return $client;
+            };
+        };
+
+        $container['proxy'] = function (Container $c)
+        {
+            return $this->app->options()['smodders_tgcore__proxy'];
         };
     }
     
+    /**
+     * Initializes a items in container for Bot Data accessing.
+     *
+     * @param Container $container
+     */
+    protected function initializeBotData(Container $container)
+    {
+        $container['bot'] = function (Container $c)
+        {
+            /** @var \XF\Entity\ConnectedAccountProvider $telegramProviderEntity */
+            $telegramProviderEntity = $this->app->em()->find('XF:ConnectedAccountProvider', 'smodders_telegram');
+            if (!$telegramProviderEntity)
+            {
+                throw new \RuntimeException("Can't find Connected Account Provider. Is this AddOn installed?");
+            }
+
+            /** @var \SModders\TelegramCore\ConnectedAccount\Provider\Telegram $telegramProvider */
+            $telegramProvider = $telegramProviderEntity->getHandler();
+            if (!$telegramProvider)
+            {
+                throw new \RuntimeException("Can't create instance for Telegram provider.");
+            }
+    
+            return array_replace($telegramProvider->getDefaultOptions(), $telegramProviderEntity->options);
+        };
+
+        $container['bot.name'] = function (Container $c)
+        {
+            return $c['bot']['name'];
+        };
+
+        $container['bot.token'] = function (Container $c)
+        {
+            return $c['bot']['token'];
+        };
+    }
+
     /**
      * @param $type
      * @param AbstractProvider $provider
@@ -96,6 +151,34 @@ class Telegram extends AbstractSubContainer
     {
         return $this->container->create('authMethod', $type, ['provider' => $provider]);
     }
-
     
+    /**
+     * @param null|string $token
+     * @return \TelegramBot\Api\BotApi
+     */
+    public function api($token = null)
+    {
+        $container = $this->container;
+        if ($token === null)
+        {
+            $token = $container['bot.token'];
+        }
+
+        return $container['api']($token);
+    }
+    
+    /**
+     * @param null|string $token
+     * @return \TelegramBot\Api\Client
+     */
+    public function client($token = null)
+    {
+        $container = $this->container;
+        if ($token === null)
+        {
+            $token = $container['bot.token'];
+        }
+    
+        return $container['client']($token);
+    }
 }
